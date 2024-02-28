d257c0c22b6cfb0421dfcc48321013acfea1e679
sean15535 <sean15535@gmail.com>
2024-02-28 14:09:41 +0100

    updates on the user input validation code

--- User Input Validation.py
+++ User Input Validation.py
@@ -1,18 +1,18 @@
 print("Welcome to Sportybet")
-print("Please Sign up \nNote that the site is only eligible for people above 18+")
+print("Please Sign up\nNote that the site is only eligible for people above 18+")
 
-response = str(input("Enter your Name: ")).upper()
-print("Enter your Gender")
-response = str(input("Gender: ")).upper()
+name = input("Enter your Name: ").upper()
+gender = input("Enter your Gender: ").upper()
 
-valid = False
-while not valid:
+valid_age = False
+while not valid_age:
     age = input("Enter your age: ")
-    if age.isdigit() and int(age) >= 18 and int(age) <=60:
-        print("You are eligible.Kindly proceed to the website")
-        valid = True
-        break
-else:
-        print("Please enter a valid age (must be at least 18 years old).")
-    
-    
+    if age.isdigit():
+        age = int(age)
+        if 18 <= age <= 60:
+            print("You are eligible. Kindly proceed to the website.")
+            valid_age = True
+        else:
+            print("Sorry, you are not eligible due to age restrictions (must be between 18 and 60).")
+    else:
+        print("Please enter a valid age (must be a number).")
