6ed202483b2578f7d1ba03955cddb7366babe389
sean15535 <sean15535@gmail.com>
2024-02-28 14:21:24 +0100

    fixes

--- User Input Validation.py
+++ User Input Validation.py
@@ -6,13 +6,11 @@ gender = input("Enter your Gender: ").upper()
 
 valid_age = False
 while not valid_age:
-    age = input("Enter your age: ")
-    if age.isdigit():
-        age = int(age)
-        if 18 <= age <= 60:
+    age = int(input("Enter your age: "))
+    if 18 <= age <= 60:
             print("You are eligible. Kindly proceed to the website.")
             valid_age = True
-        else:
-            print("Sorry, you are not eligible due to age restrictions (must be between 18 and 60).")
     else:
-        print("Please enter a valid age (must be a number).")
+        print("Sorry, you are not eligible due to age restrictions (must be between 18 and 60).")
+        break
+
