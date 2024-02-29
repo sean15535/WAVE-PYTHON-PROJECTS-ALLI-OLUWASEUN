aab9337cce229017ce7b92a9579b023f0e4af9ab
sean15535 <sean15535@gmail.com>
2024-02-29 18:37:30 +0100

    learining dictionary

---
+++ dictionary.py
@@ -0,0 +1,6 @@
+response ={ "ikotun" : 200, "banana": 3, "orange":1}
+print(response["ikotun"])
+#adding a value to a dictionary
+response["grape"] = 4
+print(response)
+#modifying a value in a dictionary
