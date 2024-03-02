d813616e2ca9fc0747536cfe215e83cd4029b2a5
sean15535 <sean15535@gmail.com>
2024-03-02 16:10:50 +0100

    learning dictionary

--- dictionary.py
+++ dictionary.py
@@ -1,6 +1,15 @@
-response ={ "ikotun" : 200, "banana": 3, "orange":1}
-print(response["ikotun"])
+my_dict = {'name': 'John', 'age': 30, 'city': 'New York'}
+print(my_dict['name'])
 #adding a value to a dictionary
-response["grape"] = 4
-print(response)
+my_dict["email"] = "john@mail.com"
 #modifying a value in a dictionary
+my_dict['age'] = 32
+print(my_dict)
+#removing elements from the dictionary using the del keyword or the pop() method:
+del my_dict["age"]
+print(my_dict)
+#using pop method to remove elements in dictionary
+my_dict.pop("city")
+print(my_dict)
+#Iterating Over a Dictionary using key()
+
