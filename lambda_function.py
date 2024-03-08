b75cd95a3d56d24937f5391cbb9599d9318b279e
sean15535 <sean15535@gmail.com>
2024-03-08 16:35:14 +0100

    learning lambda function

--- lambda_function.py
+++ lambda_function.py
@@ -2,7 +2,16 @@
 # x = lambda a: a + 10
 # print(x(5))
 # Lambda functions can take multiple arguments.
-x = lambda a, b: a * b
-print(x(5, 6))
+# x = lambda a, b: a * b
+# print(x(5, 6))
+
+#lambda can take a variable and act as a function
+# def myfunc(n):
+#     return lambda a: a * n
+
+# x = myfunc(2)
+# print(x(11))
+# y = myfunc(3)
+# print(y(11))
 
 
