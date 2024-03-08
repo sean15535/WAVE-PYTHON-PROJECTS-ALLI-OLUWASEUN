294a5a6230f6fdd20bcc92117eb288dedc4a9dcf
sean15535 <sean15535@gmail.com>
2024-03-08 16:17:01 +0100

    learning lambda

--- lambda_function.py
+++ lambda_function.py
@@ -1,4 +1,8 @@
 # A lambda function is defined using the keyword `lambda`, followed by the arguments and the expression.
-x = lambda a: a + 10
-print(x(5))
+# x = lambda a: a + 10
+# print(x(5))
+# Lambda functions can take multiple arguments.
+x = lambda a, b: a * b
+print(x(5, 6))
+
 
