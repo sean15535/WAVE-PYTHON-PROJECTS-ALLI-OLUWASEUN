e08b9893e88f83686f342aa340ce8254a3e0a630
sean15535 <sean15535@gmail.com>
2024-03-12 12:25:27 +0100

    learning insights

--- playground.py
+++ playground.py
@@ -4,6 +4,12 @@ print(" this is a line \nthis is another new line")
 #you can use raw strings by adding an r before the first quote:
 print("C:\some\name")
 print(r"C:\some\name")
-
-
-
+# using triple-quotes: """...""" or '''...'''. and add \
+#you can use it to print multiple lines
+print("""\ 
+      This is  a line                           AS YOU GO
+      this is another line       as you go 
+      
+     """ )
+#you can repeat a string using *
+print(3 * "processing ")
