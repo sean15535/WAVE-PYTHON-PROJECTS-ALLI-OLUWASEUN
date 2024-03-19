d3fc311257149664f9a99e3c8e57320006bc3ce5
sean15535 <sean15535@gmail.com>
2024-03-19 15:19:43 +0100

    learning more about tuples

--- tuple.py
+++ tuple.py
@@ -1,13 +1,17 @@
 # a = (1,2,3)
 # b=(4,5,6)
 # print(a+b)
-fruit = ("banana", True, 23, 45.76)
-new_fruit = list(fruit)
-print(fruit)
-print(type(fruit))
-print(new_fruit)
-print(type(new_fruit))
+# fruit = ("banana", True, 23, 45.76)
+# new_fruit = list(fruit)
+# print(fruit)
+# print(type(fruit))
+# print(new_fruit)
+# print(type(new_fruit))
 
-tuple = [(1,2), (2,3), (1,6), (3,2),(4,1)]
-for index, value in tuple:
-    print(index , value)
+# tuple = [(1,2), (2,3), (1,6), (3,2),(4,1)]
+# print(type(tuple))
+# for a, b in tuple:
+#     print(a,b )
+a = (1,2,3)
+b=(4,5,6)
+print(a * 2)
