3ae7a67bbba65c72d7d05d7b819e20e205203804
sean15535 <sean15535@gmail.com>
2024-03-22 16:20:36 +0100

    learning tkinter

---
+++ trying_ tkinter.py
@@ -0,0 +1,21 @@
+# from tkinter import *
+# from tkinter import ttk
+
+# root = Tk()
+# frm = ttk.Frame(root, padding=10)
+# frm.grid()
+# ttk.Label(frm, text="Hello World!").grid(column=0, row=0)
+# ttk.Button(frm, text="Quit", command=root.destroy).grid(column=1, row=0)
+# root.mainloop()
+
+from tkinter import *
+window = Tk()
+window.minsize(width=500, height=500)
+window.title('Python App')
+word = Label(text = "Enter Username")
+word.pack()
+input = Entry()
+input.pack()
+button =Button(text="Click")
+button.pack()
+window.mainloop()
